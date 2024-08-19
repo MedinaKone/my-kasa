c46c422d2012590384aa774aad37f2f40d741897
Medina Kone <kone-medina@hotmail.com>
2024-08-19 11:57:03 +0200

    Correction sémantique pages "Home" et "About")

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,21 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import Banner from "../components/Banner"
-import Collapse from "../components/Collapse"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Banner from "../components/Banner";
+import Collapse from "../components/Collapse";
 
-const About = ()=> {
-    return(
-       <>
-       <Header /> 
-       <section className="main-content">
-       <Banner type="about" />
-       <Collapse />
-       </section>
-        <Footer />
-        </>
-    )
-}
+const About = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Banner type="about" />
+        <section className="main-content">
+          <Collapse />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,24 @@
 import Card from "../components/Card";
-import Banner from "../components/Banner"
+import Banner from "../components/Banner";
 import logements from "../data/logements.json"; // Assurez-vous que le chemin est correct
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-const Home = ()=> {
-    return(
-        <>
-        <Header />
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Banner type="home" />
+        <section className="card-section">
+          <div className="card-container">
+            <Card logements={logements} />
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-        <section className="main-content">
-       <Banner type="home"/> 
-       <div className="card-container">
-
-       <Card logements={logements}/>
-       </div>
-       </section>
-
-       <Footer />
-       </>
-    )
-}
-
-export default Home
+export default Home;
